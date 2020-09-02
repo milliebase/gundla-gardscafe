036bef3a72111323f8ce54e0e54d12de916b00e7
milliebase <betsyalva@milliebase.se>
2020-09-02 12:19:16 +0200

    Init files

---
+++ studio/schemas/menu.js
@@ -0,0 +1,13 @@
+export default {
+  name: "menu",
+  title: "Navigation menu",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      description: "Title of page",
+    },
+  ],
+};
