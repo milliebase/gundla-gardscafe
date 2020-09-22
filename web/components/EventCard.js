fcb99344e4a17fa9f8dc8196a37392f9f2220e45
oskarjoss <oskar.sjolin.joss@gmail.com>
2020-09-22 14:23:29 +0200

    styling and Link eventCards

--- web/components/EventCard.js
+++ web/components/EventCard.js
@@ -1,25 +1,49 @@
 import styled from "styled-components";
 import Button from "./Button";
+import Link from "next/link";
 
 const StyledEventCard = styled.div`
   margin: 0 20px 40px 20px;
+  padding-bottom: 16px;
   background-color: var(--gundla-paper);
+  section {
+    height: 206px;
+    width: 100%;
+  }
   img {
     width: 100%;
-    height: 206px;
+    height: 100%;
+    object-fit: cover;
+  }
+  div {
+    padding: 16px;
+    * {
+      margin: 0;
+    }
+  }
+  Button {
+    margin-left: 16px;
+  }
+  h3 {
+    margin-bottom: 6px;
+    font-weight: normal;
   }
 `;
 
-const EventCard = ({ item }) => {
+const EventCard = ({ item, path }) => {
   return (
     <StyledEventCard>
-      <img src={item.imageUrl} />
+      <section>
+        <img src={item.imageUrl} />
+      </section>
       <div>
         <p>{item.date}</p>
-        <h2>{item.title}</h2>
+        <h3>{item.title}</h3>
         <p>{item.description}</p>
-        <Button text="knapp" />
       </div>
+      <Link href={`/${path}/${item._id}`} passHref>
+        <Button text="läs mer" />
+      </Link>
     </StyledEventCard>
   );
 };
--- web/pages/activities/[slug].js
+++ web/pages/activities/[slug].js
@@ -10,18 +10,18 @@ const SingleEvent = (props) => {
 };
 
 export async function getStaticPaths() {
-  const query = `*[_type == "eventList"]`;
-  const events = await client.fetch(query);
+  const query = `*[_type == "activityList"]`;
+  const activities = await client.fetch(query);
 
-  const paths = events.map((event) => ({
-    params: { slug: event._id },
+  const paths = activities.map((activity) => ({
+    params: { slug: activity._id },
   }));
 
   return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }) {
-  const query = `*[_type == 'eventList' && _id == '${params.slug}']`;
+  const query = `*[_type == 'activityList' && _id == '${params.slug}']`;
   const content = await client.fetch(query);
 
   return {
