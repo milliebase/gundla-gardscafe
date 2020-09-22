e9a9dad78d7a56dacd2e2e63e9eb32948ceda8a3
milliebase <betsyalva@milliebase.se>
2020-09-22 12:53:36 +0200

    Added history component

--- web/components/History/index.js
+++ web/components/History/index.js
@@ -15,8 +15,46 @@ const StyledHistory = styled.div`
   }
 
   @media (min-width: 992px) {
+    position: relative;
+
     .history {
+      position: absolute;
       width: 50%;
+    }
+  }
+`;
+
+const StyledFlowers = styled.div`
+  display: none;
+
+  @media (min-width: 992px) {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: space-between;
+    z-index: -10;
+
+    div {
+      width: 100%;
+      height: 50%;
+    }
+
+    div:first-of-type {
+      img {
+        margin-top: -10px;
+        margin-left: -50px;
+      }
+    }
+
+    div:last-of-type {
+      display: flex;
+      justify-content: flex-end;
+
+      img {
+        margin-bottom: -100px;
+        margin-right: 50px;
+      }
     }
   }
 `;
@@ -28,6 +66,16 @@ const History = ({ heading, text }) => {
         <DecorHeading heading={heading} dark />
         <PortableText blocks={text} />
       </div>
+
+      <StyledFlowers>
+        <div>
+          <img src="/assets/rose.png" />
+        </div>
+
+        <div>
+          <img src="/assets/orange-flower.png" />
+        </div>
+      </StyledFlowers>
     </StyledHistory>
   );
 };
